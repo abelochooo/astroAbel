a68acf19220105995d0edfb6b878711c9d818fb3
abelochooo <acanjim2401@g.educaand.es>
2026-08-19 13:28:34 +0200

    Committ

--- script.js
+++ script.js
@@ -57,9 +57,9 @@ document.getElementById("activar").addEventListener("click", async () => {
         if (permiso !== "granted") return;
     }
 
-    window.addEventListener("deviceorientation", (e) => {
-        document.getElementById("alpha").textContent = e.alpha;
-        document.getElementById("beta").textContent = e.beta;
-        document.getElementById("gamma").textContent = e.gamma;
+    window.addEventListener("deviceorientationabsolute", (e) => {
+        document.getElementById("alpha").textContent = e.alpha?.toFixed(1);
+        document.getElementById("beta").textContent = e.beta?.toFixed(1);
+        document.getElementById("gamma").textContent = e.gamma?.toFixed(1);
     });
 });
