e3c4a8d202ba0a23d01efbaeb1e99954d8c6eff4
abelochooo <acanjim2401@g.educaand.es>
2026-08-19 12:39:36 +0200

    Primer commit

---
+++ script.js
@@ -0,0 +1,40 @@
+const texto1 = document.getElementById("ubicacionMensaje");
+const ubicacion = document.getElementById("ubicacionUsuario");
+const ubicacionDiv = document.getElementById("ubicacionDiv");
+
+const texto = "Tu ubicación es";
+let i = 0;
+
+function escribir() {
+    if (i < texto.length) {
+        texto1.textContent += texto[i];
+        i++;
+        setTimeout(escribir, 100);
+    } else {
+        obtenerUbicacion();
+    }
+}
+
+async function obtenerUbicacion() {
+    navigator.geolocation.getCurrentPosition(async ({ coords }) => {
+
+        const resultado = await fetch(
+            `https://nominatim.openstreetmap.org/reverse?lat=${coords.latitude}&lon=${coords.longitude}&format=json`
+        );
+
+        const informacion = await resultado.json();
+
+        const { city, town, village, municipality, country } = informacion.address;
+
+        const ciudad = city || town || village || municipality;
+
+        ubicacion.textContent = `${ciudad}, ${country}`;
+
+
+        setTimeout(() => {
+            ubicacionDiv.remove();
+        }, 3000);
+    });
+}
+
+escribir();
